6db1ae3b08b0bfd4a8dd730d5e20214347ca3bce
Uluru92 <jorddy.castro@gmail.com>
2025-09-11 12:12:44 -0600

    class Student included

--- models.py
+++ models.py
@@ -1,6 +1,33 @@
+class Student:
+    ''''Info about Students'''
+    def __init__(self, student_id, name):
+        self.student_id = student_id
+        self.name = name
 
+class Bus:
+    '''Info about Bus'''
+    def __init__(self, bus_id):
+        self.bus_id = bus_id
+        self.locations = [] # list of locations
+        self.students_onboard = []  # list of students inside the bus
+
+    def add_location(self, lat, lng, timestamp):
+        loc = BusLocation(self.bus_id, lat, lng, timestamp)
+        self.locations.append(loc)
+
+    def latest_location(self):
+        return self.locations[-1] if self.locations else None
+    
+    def board_student(self, student: Student):
+        if student not in self.students_onboard:
+            self.students_onboard.append(student)
+            print(f"{student.name} boarded {self.bus_id}")
+
+    def list_students(self):
+        return [s.name for s in self.students_onboard]
 
 class BusLocation:
+    '''Info about every point location received'''
     def __init__(self, bus_id, lat, lng, timestamp):
         self.bus_id = bus_id
         self.lat = lat
@@ -19,6 +46,7 @@ class BusLocation:
         }
 
 class BusTracker:
+    '''Info about all coords from a bus'''
     def __init__(self):
         self.coords_by_bus = {}
 
@@ -33,3 +61,5 @@ class BusTracker:
         if bus_id in self.coords_by_bus and self.coords_by_bus[bus_id]:
             return self.coords_by_bus[bus_id][-1]
         return None
+
+
