bda7e96cfe7da4c3318b1b663d97f8ff5789e20a
Uluru92 <jorddy.castro@gmail.com>
2025-10-04 16:05:28 -0600

    test_get_db_connection PASSED

---
+++ test_bus_tracker.py
@@ -0,0 +1,19 @@
+import unittest
+from bus_tracker import *
+
+class TestRescrape(unittest.TestCase):
+
+    def test_get_db_connection(self):
+        conn = get_db_connection()
+        self.assertIsNotNone(conn,"failed to connect to DB")
+
+        cursor = conn.cursor()
+        cursor.execute("SELECT 1,2,3,4,5")
+        result = cursor.fetchall()
+        self.assertEqual(result[0][3],4,"The query result should be 3")
+
+        cursor.close()
+        conn.close()
+
+if __name__ == "__main__":
+    unittest.main()
